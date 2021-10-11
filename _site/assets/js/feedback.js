da6beb1f559e8ebd3b123b74f798fbf972a13521
Robert <rcrooks@brightcove.com>
2021-10-11 12:01:56 -0400

    update translations

--- _site/assets/js/feedback.js
+++ _site/assets/js/feedback.js
@@ -1,5 +1,5 @@
 var fby = fby || [];
-fby.push(['showTab', {id: '15071', position: 'right', color: 'rgb(4, 155, 179)'}]);
+fby.push(['showTab', {id: '15071', position: 'right', color: 'rgb(211, 61, 0)'}]);
 (function () {
     var f = document.createElement('script'); f.type = 'text/javascript'; f.async = true;
     f.src = '//cdn.feedbackify.com/f.js';
--- assets/js/talla-init.js
+++ assets/js/talla-init.js
@@ -1,16 +1,20 @@
-var appID = "00c26be3-4421-41e6-bb00-1dee68740f67";
+---
+layout: null
+---
+
+var appID = "{{ site.talla_appID }}";
 // token for anonymous profile
 var token =
-'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1ODI4MDQzNDF9.RYf2x65FXvdDURzM h00i76KfUrYlsDLsw6mVuEv6zmE';
+'{{ site.talla_token }}';
 // -----
   Talla.config = Talla.init(
     appID,
     Talla.OptionJWTCredentials(token),
-    Talla.OptionBotName("Brightcove Audience Doc Search"),
+    Talla.OptionBotName("{{ site.product }} Doc Search"),
     Talla.OptionAnonymous(true),
-    Talla.OptionThemeColor('rgb(4, 155, 179)'),
-    Talla.OptionDisplayName('Search Brightcove Audience Docs'),
-    Talla.OptionKnowledgeGroups(['d89f9dbb-d575-4bf5-bb85-8df0fe0fcdf4']),
+    Talla.OptionThemeColor('{{ site.product_color }}'),
+    Talla.OptionDisplayName('Search {{ site.product }} Docs'),
+    Talla.OptionKnowledgeGroups(['{{ site.talla_knowledgegroup }}']),
     Talla.OptionWideBeacon(true),
     Talla.OptionBeaconCTA("Have a question?"),
     Talla.OptionOpenIcon('https://support.brightcove.com/site-assets/images/site/search.png'),
